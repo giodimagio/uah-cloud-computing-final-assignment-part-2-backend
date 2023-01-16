35999bb63b7e0e4049a8e7827102148aef1bdb63
giodimagio <giodimagio@gmail.com>
2023-01-16 21:28:07 +0100

    [app.py] Flask jsonify to serialize args as JSON

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import datetime
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -7,14 +7,14 @@ app = Flask(__name__)
 @app.route("/microservice-1")
 @cross_origin()
 def microservice_1():
-    return ("Microservice 1 - Version 1 - Time: " + str(datetime.datetime.now()))
+    return jsonify(message="Microservice 1 - Version 1", timestamp=str(datetime.datetime.now()))
 
 @app.route("/microservice-2")
 @cross_origin()
 def microservice_2():
-    return ("Microservice 2 - Version 1 - Time: " + str(datetime.datetime.now()))
+    return jsonify(message="Microservice 2 - Version 1", timestamp=str(datetime.datetime.now()))
 
 @app.route("/microservice-3")
 @cross_origin()
 def microservice_3():
-    return ("Microservice 3 - Version 1 - Time: " + str(datetime.datetime.now()))
+    return jsonify(message="Microservice 3 - Version 1", timestamp=str(datetime.datetime.now()))
