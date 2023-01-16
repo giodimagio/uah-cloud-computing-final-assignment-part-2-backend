c8d504e7f10507781406d795185208dedb95f2ab
giodimagio <giodimagio@gmail.com>
2023-01-16 21:46:53 +0100

    [app.py] Refactor microservices Flask-RESTful way

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
 import datetime
 from flask import Flask, jsonify
 from flask_cors import CORS, cross_origin
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/microservice-1")
-@cross_origin()
-def microservice_1():
-    return jsonify(message="Microservice 1 - Version 1", timestamp=str(datetime.datetime.now()))
+class Microservice1(Resource):
+    @cross_origin()
+    def get(self):
+        return jsonify(message="Microservice 1 - Version 1", timestamp=str(datetime.datetime.now()))
 
-@app.route("/microservice-2")
-@cross_origin()
-def microservice_2():
-    return jsonify(message="Microservice 2 - Version 1", timestamp=str(datetime.datetime.now()))
+class Microservice2(Resource):
+    @cross_origin()
+    def get(self):
+        return jsonify(message="Microservice 2 - Version 1", timestamp=str(datetime.datetime.now()))
 
-@app.route("/microservice-3")
-@cross_origin()
-def microservice_3():
-    return jsonify(message="Microservice 3 - Version 1", timestamp=str(datetime.datetime.now()))
+class Microservice3(Resource):
+    @cross_origin()
+    def get(self):
+        return jsonify(message="Microservice 3 - Version 1", timestamp=str(datetime.datetime.now()))
+
+api.add_resource(Microservice1, '/microservice-1')
+api.add_resource(Microservice2, '/microservice-2')
+api.add_resource(Microservice3, '/microservice-3')
