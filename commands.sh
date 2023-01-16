a5765b4713f299ade177ef5eb0a9422d7f202dde
giodimagio <giodimagio@gmail.com>
2023-01-17 00:37:40 +0100

    [commands.sh] A file to store shell commands used in the project

---
+++ commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#################### Commands used for the project ####################
+
+# ------------------ Development ------------------ #
+
+# ------------------ Production ------------------ #
+
+# ------------------ Others ------------------ #
+
+## Pipenv Workflow: https://pipenv-es.readthedocs.io/es/latest/basics.html#example-pipenv-workflow
+
+    ### Start a project with pipenv
+        #### pipenv install
+        #### pipenv shell
+        #### python --version
