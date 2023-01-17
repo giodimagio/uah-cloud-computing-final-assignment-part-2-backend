b897d4d5f7ec4b90f028cf610b21d57ba6dbe3fa
giodimagio <giodimagio@gmail.com>
2023-01-17 19:12:33 +0100

    [commands.sh] Add project commands at this point

--- commands.sh
+++ commands.sh
@@ -4,13 +4,56 @@
 
 # ------------------ Development ------------------ #
 
+    ## (Locally) Run each microservice on WSL2:
+
+        ### Inside pipenv shell
+        flask --app ./microservice1/microservice_1.py --debug run --port=5001
+        flask --app ./microservice2/microservice_2.py --debug run --port=5002
+        flask --app ./microservice3/microservice_3.py --debug run --port=5003
+
+        ### Outside pipenv shell
+        python -m flask --app ./microservice1/microservice_1.py --debug run --port=5001
+        python -m flask --app ./microservice2/microservice_2.py --debug run --port=5002
+        python -m flask --app ./microservice3/microservice_3.py --debug run --port=5003
+
+    ## (Locally) Run each microservice with Docker:
+
+        ### Build images
+        docker image build -t giodimagio/microservice1:v1.0 ./microservice1/
+        docker image build -t giodimagio/microservice2:v1.0 ./microservice2/
+        docker image build -t giodimagio/microservice3:v1.0 ./microservice3/
+
+        ### Remove images
+        docker build -t giodimagio/microservice1:v1.0 ./microservice1/
+        docker build -t giodimagio/microservice2:v1.0 ./microservice2/
+        docker build -t giodimagio/microservice3:v1.0 ./microservice3/
+
+        ### Run containers
+        docker container run -d -p 5010:5000 --name microservice1 giodimagio/microservice1:v1.0
+        docker container run -d -p 5020:5000 --name microservice2 giodimagio/microservice2:v1.0
+        docker container run -d -p 5030:5000 --name microservice3 giodimagio/microservice3:v1.0
+
+        ### Execute commands in containers
+        docker container exec -it microservice1 /bin/bash
+        docker container exec -it microservice2 /bin/bash
+        docker container exec -it microservice3 /bin/bash
+
 # ------------------ Production ------------------ #
 
 # ------------------ Others ------------------ #
 
-## Pipenv Workflow: https://pipenv-es.readthedocs.io/es/latest/basics.html#example-pipenv-workflow
+    ## Pipenv - Workflow: https://pipenv-es.readthedocs.io/es/latest/basics.html#example-pipenv-workflow
+    pipenv install
+    pipenv shell
+    python --version
 
-    ### Start a project with pipenv
-        #### pipenv install
-        #### pipenv shell
-        #### python --version
+    ## Pipenv - install <package>
+    pipenv install flask
+    pipenv install flask-cors
+    pipenv install flask-restful
+
+    ## Git - push
+    git push origin main
+
+    ## SSH - Authentication with GitHub
+    eval $(ssh-agent) && ssh-add ~/.ssh/id_rsa_github_giodimagio_wsl2
