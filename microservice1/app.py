18be94fa9aee576126174c42ba1ed6db41fd1b77
giodimagio <giodimagio@gmail.com>
2023-01-21 20:45:09 +0100

    [app.py] Rename Python app file

--- microservice1/app.py
+++ microservice1/app.py
@@ -9,6 +9,9 @@ api = Api(app)
 class Microservice1(Resource):
     @cross_origin()
     def get(self):
-        return jsonify(message="Microservice 1 - Version 1", timestamp=str(datetime.datetime.now()))
+        return jsonify(message="MicroService 1 - Version 1", timestamp=str(datetime.datetime.now()))
     
-api.add_resource(Microservice1, '/microservice-1')
+api.add_resource(Microservice1, '/api/v1/microservice1')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
