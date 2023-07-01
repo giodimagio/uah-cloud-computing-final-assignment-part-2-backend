4284287598c294428f1434665409e327a78dcfdc
giodimagio <giodimagio@gmail.com>
2023-07-01 14:38:18 +0200

    [sh] Clean all deployments

---
+++ development-clean-k8s-deployments.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#################### Cleaning script to delete all deployments ####################
+
+# ------------------ Development ------------------ #
+
+kubectl delete -f development-deploy-k8s-microservice1-v1.yaml -n uah-development
+
+kubectl delete -f development-deploy-istio-gateway.yaml -n uah-development
+
+kubectl delete gateway ingress-gateway -n uah-development
+
+kubectl delete pods --all -n uah-development
+
+kubectl delete services --all -n uah-development
+
+kubectl delete deployments --all -n uah-development
+
+kubectl delete virtualservices --all -n uah-development
+
+kubectl delete destinationrules --all -n uah-development
+
+kubectl delete serviceaccounts --all -n uah-development
+
+kubectl delete gateways --all -n uah-development
+
+kubectl delete namespace uah-development 
